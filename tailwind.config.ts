30c276c006199d6048228f6a65ea2c803ca26b09
Dias Junior <reinaldo.diasjunior@gmail.com>
2024-03-21 17:21:36 +0100

    chore(tailwind): add colors to tailwind theme config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        normal: "#EAE8E1",
+        bug: "#BAC226",
+        dark: "#756D63",
+        dragon: "#A99FE9",
+        electric: "#F4BE6E",
+        fairy: "#F4CFF0",
+        fighting: "#B27650",
+        fire: "#FF7F70",
+        flying: "#AFC7FF",
+        ghost: "#A5A5C9",
+        grass: "#9FD360",
+        ground: "#D4B881",
+        ice: "#C9F3FF",
+        poison: "#B58DBF",
+        psychic: "#F1A9C4",
+        rock: "#93836C",
+        steel: "#ABB4B8",
+        water: "#72BBFF",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
