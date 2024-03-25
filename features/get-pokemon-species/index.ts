1fb8a7be6931772168aed218658661bb3ed53260
Dias Junior <reinaldo.diasjunior@gmail.com>
2024-03-25 21:06:26 +0100

    refact(features): refact lsit pokemon species

--- features/get-pokemon-species/index.ts
+++ features/get-pokemon-species/index.ts
@@ -39,6 +39,7 @@ export const getPokemonSpecies = async (
     const speciesData = await speciesResponse.json();
 
     const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`;
+
     return successResponse<PokemonSpecies>({
       id: data.id,
       name: data.name,
