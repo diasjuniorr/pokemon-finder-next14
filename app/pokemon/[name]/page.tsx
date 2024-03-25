4de6e1435b1147eeb64ac7f4933b4888ebf0725f
Dias Junior <reinaldo.diasjunior@gmail.com>
2024-03-25 20:35:40 +0100

    refact(features): remove duplicate feature

--- app/pokemon/[name]/page.tsx
+++ app/pokemon/[name]/page.tsx
@@ -1,52 +1,55 @@
-import { getPokemonEvolutions } from "@/features/get-pokemon-evolutions";
 import { PokemonDetailsCard } from "./components/pokemon-details-card";
 import { HabitatCard } from "./components/habitat-card";
 import { FunFactCard } from "./components/fun-fact-card";
 import NavigateBack from "./components/navigate-back";
 import { ErrorPage } from "@/shared/components/error-page";
+import { getPokemonEvolutionChain } from "@/features/get-pokemon-evolution-chain";
 
 const PokemonPage = async ({ params }: { params: { name: string } }) => {
   const { name } = params;
-  const [error, result] = await getPokemonEvolutions(name);
+  const [error, result] = await getPokemonEvolutionChain(name);
 
   if (error.hasError) {
     return <ErrorPage />;
   }
 
-  const { pokemon, evolutionChain } = result.data!;
+  const { data } = result;
+  const pagePokemon = data!.find((pokemon) => pokemon.name === name);
 
   return (
     <div className="bg-slate-100 min-h-screen">
       <div className=" mx-auto px-10 w-fit">
         <h1 className="pt-6 pb-2 mb-6 text-4xl font-extrabold leading-none tracking-tight text-gray-900  dark:text-white">
-          {`${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}`}{" "}
-          Evolution Chain
+          {`${name.charAt(0).toUpperCase()}${name.slice(1)}`} Evolution Chain
         </h1>
         <NavigateBack />
         <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 max-w-screen-lg ">
-          {evolutionChain.map((evolution) => (
-            <PokemonDetailsCard
-              key={evolution.name}
-              isSelected={evolution.name === pokemon.name}
-              evolution={evolution}
-            />
-          ))}
+          {data &&
+            data.map((evolution) => (
+              <PokemonDetailsCard
+                key={evolution.name}
+                isSelected={evolution.name === name}
+                evolution={evolution}
+              />
+            ))}
         </div>
         <h1 className="mb-4 pt-6 pb-6 text-4xl font-extrabold leading-none tracking-tight text-gray-900  dark:text-white">
-          {`Other info about ${pokemon.name
-            .charAt(0)
-            .toUpperCase()}${pokemon.name.slice(1)}`}
+          {`Other info about ${name.charAt(0).toUpperCase()}${name.slice(1)}`}
         </h1>
-        <div className="flex flex-col gap-4 pb-10 md:flex-row md:gap-4">
-          <HabitatCard pokemon={pokemon} />
-          <FunFactCard
-            funFact={
-              getRandomItem(
-                pokemon.funFacts.filter((fact) => fact.language.name === "en")
-              ).flavor_text
-            }
-          />
-        </div>
+        {pagePokemon && (
+          <div className="flex flex-col gap-4 pb-10 md:flex-row md:gap-4">
+            <HabitatCard pokemon={pagePokemon} />
+            <FunFactCard
+              funFact={
+                getRandomItem(
+                  pagePokemon.funFacts.filter(
+                    (fact) => fact.language.name === "en"
+                  )
+                ).flavor_text
+              }
+            />
+          </div>
+        )}
       </div>
     </div>
   );
