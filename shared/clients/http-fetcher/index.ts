90434bf8f5cf4a607258098361c1c20e68c20435
Dias Junior <reinaldo.diasjunior@gmail.com>
2024-03-27 09:14:47 +0100

    refact(clients): handle http responses different than 200 and 404

--- shared/clients/http-fetcher/index.ts
+++ shared/clients/http-fetcher/index.ts
@@ -14,12 +14,16 @@ export async function fetchJson<T>(url: string): Promise<FetchJsonResponse<T>> {
   try {
     const response = await fetch(url);
     if (response.status === 404) {
-      return errorResponse(new Error("Resource not found"), 404);
+      return errorResponse(new Error("resource not found"), 404);
+    }
+
+    if (!response.ok) {
+      return errorResponse(new Error("error fetching JSON"));
     }
 
     return successResponse<T>(await response.json());
   } catch (err) {
     console.log("Error fetching JSON", err);
-    return errorResponse(new Error("Error fetching JSON"));
+    return errorResponse(new Error("error fetching JSON"));
   }
 }
